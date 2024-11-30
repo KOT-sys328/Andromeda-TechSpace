e4b61ce55b72fa1be8cfff0c8abdd8e09c20c697
Konstantin <kostya12l34g@gmail.com>
2024-11-30 18:42:30 +0300

    player

--- SuperInovationGame2024/Assets/Scripts/Player.cs
+++ SuperInovationGame2024/Assets/Scripts/Player.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
+using static UnityEditor.PlayerSettings;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] int health;
     [SerializeField] float speed;
+    private RectTransform rect;
+    private int layerMask = 7;
     void Start()
     {
+        rect = GetComponent<RectTransform>();
     }
     void Update()
     {
-        transform.position = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            rect.position = hit.point;
+        }
     }
 }
